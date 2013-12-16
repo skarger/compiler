d01b0e720bd7ff9db4e5369fc765102e976d63a6
Stephen Karger <stephen.karger@gmail.com>
2013-12-16 16:28:13 -0500

    begin printing IR nodes as MIPS

--- src/mips/mips-utils.c
+++ src/mips/mips-utils.c
@@ -8,6 +8,8 @@ static const char *builtin_print_int = "print_int:\n"
 "    jr $ra            # return to caller\n";
 
 void print_global_variables(FILE *out, SymbolTable *st);
+void print_functions(FILE *out, SymbolTableContainer *stc, IrList *irl);
+void ir_to_mips(FILE *out, IrNode *irn);
 
 void compute_mips_asm(FILE *output, SymbolTableContainer *stc, IrList *irl) {
     fprintf(output, ".data\n");
@@ -19,6 +21,9 @@ void compute_mips_asm(FILE *output, SymbolTableContainer *stc, IrList *irl) {
 
     fprintf(output, "\n");
     fprintf(output, ".text\n");
+    /* print each function */
+    print_functions(output, stc, irl);
+
     fprintf(output, "\n");
     fprintf(output, "%s", builtin_print_int);
 }
@@ -31,5 +36,29 @@ void print_global_variables(FILE *out, SymbolTable *st) {
             fprintf(output, "%s: .word 0\n", get_symbol_name(s));
         }
         s = s->next;
+    }
+}
+
+void print_functions(FILE *out, SymbolTableContainer *stc, IrList *irl) {
+    IrNode *cur = irl->head;
+    while (cur != NULL) {
+        ir_to_mips(out, cur);
+        cur = cur->next;
+    }
+}
+
+void ir_to_mips(FILE *out, IrNode *irn) {
+    switch(irn->instruction) {
+        case BEGIN_PROC:
+            fprintf(out, "%s:\n", get_symbol_name(irn->s));
+            fprintf(out, "    sw    $ra, -4($fp)\n");
+            break;
+        case END_PROC:
+            fprintf(out, "    lw    $ra, -4($fp)    # restore $ra\n");
+            fprintf(out, "    jr    $ra\n");
+            break;
+        default:
+            fprintf(out, "unknown instruction\n");
+            break;
     }
 }
